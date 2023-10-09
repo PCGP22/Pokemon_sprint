9e8bc8281ee83c47dad1ce45fa585e30349f12a7
Paulo Gutiérrez <94929429+PCGP22@users.noreply.github.com>
2023-10-09 02:34:49 -0600

    Update main.tsx

--- Pokemon App/src/main.tsx
+++ Pokemon App/src/main.tsx
@@ -7,13 +7,13 @@ import { store } from "./redux/store.js";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <GoogleOAuthProvider clientId="435454517185-mteumdmd34v0rtn854fph4npai3a3lvd.apps.googleusercontent.com">
     <BrowserRouter>
-      <Provider store={store}>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </Provider>
-    </BrowserRouter>
-  </GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId="435454517185-mteumdmd34v0rtn854fph4npai3a3lvd.apps.googleusercontent.com">
+        <Provider store={store}>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </Provider>
+    </GoogleOAuthProvider>
+  </BrowserRouter>
 );
